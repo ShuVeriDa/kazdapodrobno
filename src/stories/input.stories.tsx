c9dd34134ae79f2c812a3ec0aa5f611d35cd8bdb
ShuVeriDA <bashtarov@outlook.com>
2022-04-09 17:54:02 +0300

    13 Controlled input, FLUX

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,5 +1,6 @@
 import {OnOff} from "../components/OnOff/OnOff";
 import {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'input',
@@ -29,6 +30,32 @@ export const GetValueOffUncontrolledInputByButtonPress = () => {
     }
 
     return <><input ref={inputRef} /> <button onClick={save}>save</button> - actual value: {value}</>
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
+
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.checked)
+
+    return <input type="checkbox" onChange={onChange}checked={parentValue}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => setParentValue(e.currentTarget.value)
+
+    return <select value={parentValue}>
+        <option>none</option>
+        <option value="1">Minsk</option>
+        <option value="2">Moscow</option>
+        <option value="3">Kiev</option>
+    </select>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={'It-Incubator'}/>
