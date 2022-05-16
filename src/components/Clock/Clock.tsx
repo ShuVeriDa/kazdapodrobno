06132830cbe72fae35543514ec189cc70cd68617
ShuVeriDA <bashtarov@outlook.com>
2022-05-16 19:29:45 +0300

    23 useEffect - component Clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,33 @@
+import {FC, useEffect, useState} from "react";
+
+type ClockPropsType = {
+
+}
+
+const  get2digitsString = (num: number) => num < 10 ? "0" + num : num
+
+export const Clock: FC<ClockPropsType> = (props) => {
+   const [date, setDate] = useState(new Date())
+
+   useEffect(() => {
+      const intervalID = setInterval(() => {
+         console.log('TICK')
+         setDate(new Date())
+      }, 1000)
+
+      return () => {
+         clearInterval(intervalID)
+      }
+   }, [])
+
+
+   return (
+      <div>
+         <span>{get2digitsString(date.getHours())}</span>
+         :
+         <span>{get2digitsString(date.getMinutes())}</span>
+         :
+         <span>{get2digitsString(date.getSeconds())}</span>
+      </div>
+   );
+};
